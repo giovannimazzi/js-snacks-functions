d60ee207f5536d1e8b4add8aea5b0b4769bcbaee
giovannimazzi <gioelor@libero.it>
2026-01-09 13:57:54 +0100

    risolto: 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,35 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+/**
+ *Accepts an array of strings (names) and returns an array containing the first char of each element.
+ *@param {string[]} origNames The array containing original names.
+ *@returns {string[]} The array containing the first char of each name.
+ */
+function fisrtChar(origNames) {
+  const outputArray = [];
+  for (let i = 0; i < origNames.length; i++) {
+    outputArray.push(origNames[i][0]);
+  }
+  return outputArray;
+}
 
+/**
+ *Accepts an array of strings (names) and returns an array containing the first char of each element.
+ *@param {string[]} origNames The array containing original names.
+ *@returns {string[]} The array containing the first char of each name.
+ */
+const fisrtChar_AF = (origNames) => {
+  const outputArray = [];
+  for (let i = 0; i < origNames.length; i++) {
+    outputArray.push(origNames[i][0]);
+  }
+  return outputArray;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(fisrtChar(names));
+console.log(fisrtChar_AF(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
