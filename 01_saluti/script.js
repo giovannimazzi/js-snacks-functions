dcf96d2a8cce2c05b79a99ee762e74132d99a28f
giovannimazzi <gioelor@libero.it>
2026-01-09 13:45:25 +0100

    risolto: 01_saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,26 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
+/**
+ *Accepts a string containing a name and returns an Italian greeting.
+ *@param {string} name The name to greet.
+ *@returns {string} The greeting message.
+ */
+function greetings(name) {
+  return `Ciao ${name.trim()}`;
+}
 
+/**
+ *Accepts a string containing a name and returns an Italian greeting.
+ *@param {string} name The name to greet.
+ *@returns {string} The greeting message.
+ */
+const greetings_AF = (name) => `Ciao ${name.trim()}`;
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(greetings(userName));
+console.log(greetings_AF(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
