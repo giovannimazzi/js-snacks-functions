70bd0e1316f2a6fc8d9587327fc4607afbcf7eeb
giovannimazzi <gioelor@libero.it>
2026-01-09 16:47:50 +0100

    risolto: 05_bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,45 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+/**
+ *Accepts a string containing a name and returns a different Italian greeting based on daytime:
+ *Buongiorno in the morning (until 13:00);
+ *Buon Pomeriggio in the afternoon (until 17:00);
+ *Buonasera in the evening (over 17:00)
+ *@param {string} name The name to greet.
+ *@returns {string} The greeting message.
+ */
+function daytimeGreetings(name) {
+  const dayTime = new Date().getHours();
+  let greet;
+  if (dayTime < 13) greet = "Buongiorno";
+  else if (dayTime < 17) greet = "Buon Pomeriggio";
+  else greet = "Buonasera";
+  return `${greet} ${name.trim()}`;
+}
 
+/**
+ *Accepts a string containing a name and returns a different Italian greeting based on daytime:
+ *Buongiorno in the morning (until 13:00);
+ *Buon Pomeriggio in the afternoon (until 17:00);
+ *Buonasera in the evening (over 17:00)
+ *@param {string} name The name to greet.
+ *@returns {string} The greeting message.
+ */
+const daytimeGreetings_AF = (name) => {
+  const dayTime = new Date().getHours();
+  let greet;
+  if (dayTime < 13) greet = "Buongiorno";
+  else if (dayTime < 17) greet = "Buon Pomeriggio";
+  else greet = "Buonasera";
+  return `${greet} ${name.trim()}`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(daytimeGreetings(name));
+console.log(daytimeGreetings_AF(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
